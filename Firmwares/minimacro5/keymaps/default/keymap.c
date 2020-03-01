cd3d7f73fe494c43d878582d88b29d44997cb017
ImEvwan <61602941+ImEvwan@users.noreply.github.com>
2020-03-01 14:53:40 -0500

    Update keymap.c

--- Firmwares/minimacro5/keymaps/default/keymap.c
+++ Firmwares/minimacro5/keymaps/default/keymap.c
@@ -5,9 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_ortho_1x5(
-    KC_A, KC_B, KC_C, KC_D, LT(_SECOND, KC_E) //I'm going to change this layer to have DEL, F1, Enter, F12, TG
+    KC_A, KC_B, KC_C, KC_D, TG(_SECOND) //I'm going to change this layer to have DEL, F1, Enter, F12, TG
   ),
   [_SECOND] = LAYOUT_ortho_1x5(
-    KC_F, KC_G, KC_H, KC_I, KC_J //This layer will just be arrow keys and TG
+    KC_E, KC_F, KC_G, KC_H, TG(_BASE) //This layer will just be arrow keys and TG
   ),
 };
